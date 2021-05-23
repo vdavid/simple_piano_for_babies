c84a5218f7fff3b3e6c56c927d7da28dd53dc944
David Veszelovszki <veszelovszki@gmail.com>
2021-05-23 21:13:10 +0200

    Added 4x4 gray areas

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,16 @@ class MyApp extends StatelessWidget {
       // debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello DSKjkds World!!!'),
+        body: InkWell(
+          onTap: () {
+            print("Container clicked");
+          },
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.yellow,
+            child: CustomPaint(painter: FaceOutlinePainter()),
+          ),
         ),
       ),
     );
@@ -29,4 +34,28 @@ class MyApp extends StatelessWidget {
   void turnOffNavigationBar() {
     SystemChrome.setEnabledSystemUIOverlays([]);
   }
+}
+
+class FaceOutlinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int y = 0; y < size.height; y++) {
+      for (int x = 0; x < size.width; x++) {
+        int brightness = (((x + y + 1) / 7) * 255).round();
+        Color color = Color.fromRGBO(brightness, brightness, brightness, 1.0);
+        final paint = Paint()
+          ..style = PaintingStyle.fill
+          ..strokeWidth = 0.0
+          ..color = color;
+        canvas.drawRect(
+          Rect.fromLTWH(x * size.width / 4, y * size.height / 4, size.width / 4,
+              size.height / 4),
+          paint,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceOutlinePainter oldDelegate) => false;
 }
