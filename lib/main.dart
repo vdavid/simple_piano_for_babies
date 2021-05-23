ff6d850f3d412c2e7223002fbe8c43db4556c4a4
David Veszelovszki <veszelovszki@gmail.com>
2021-05-23 22:09:19 +0200

    - Replaced the canvas with rows and columns, and 4x4 containers instead of 1. - Bound events to the keys.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ import 'package:flutter/services.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final int _rowCount = 4;
+  final int _columnCount = 4;
+
   @override
   Widget build(BuildContext context) {
     turnOffNavigationBar();
@@ -16,16 +19,31 @@ class MyApp extends StatelessWidget {
       // debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
-        body: InkWell(
+        body: Column(
+          children: _buildRows(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildRows() {
+    return List.generate(
+        _rowCount, (y) => Expanded(child: Row(children: _buildTiles(y))));
+  }
+
+  List<Widget> _buildTiles(y) {
+    return List.generate(_columnCount, (x) => _buildTile(x, y));
+  }
+
+  Widget _buildTile(int x, int y) {
+    int brightness = (((x + y + 1) / 7) * 255).round();
+    return Expanded(
+      child: Material(
+        color: Color.fromRGBO(brightness, brightness, brightness, 1.0),
+        child: InkWell(
           onTap: () {
-            print("Container clicked");
+            print("$x $y Container clicked");
           },
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.yellow,
-            child: CustomPaint(painter: FaceOutlinePainter()),
-          ),
         ),
       ),
     );
@@ -34,28 +52,4 @@ class MyApp extends StatelessWidget {
   void turnOffNavigationBar() {
     SystemChrome.setEnabledSystemUIOverlays([]);
   }
-}
-
-class FaceOutlinePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (int y = 0; y < size.height; y++) {
-      for (int x = 0; x < size.width; x++) {
-        int brightness = (((x + y + 1) / 7) * 255).round();
-        Color color = Color.fromRGBO(brightness, brightness, brightness, 1.0);
-        final paint = Paint()
-          ..style = PaintingStyle.fill
-          ..strokeWidth = 0.0
-          ..color = color;
-        canvas.drawRect(
-          Rect.fromLTWH(x * size.width / 4, y * size.height / 4, size.width / 4,
-              size.height / 4),
-          paint,
-        );
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(FaceOutlinePainter oldDelegate) => false;
 }
